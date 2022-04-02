6a5938b6538dfdc253df970170c6ffbdb62681cf
HarrisonAdamson <harryadamson2001@gmail.com>
2022-04-02 17:09:31 -0600

    Finished assessment

---
+++ profile.js
@@ -0,0 +1,11 @@
+document.getElementById("color").addEventListener("click", function() {
+    alert("Violet!");
+  });
+
+document.getElementById("place").addEventListener("click", function() {
+    alert("Teton National Park!");
+  });
+
+document.getElementById("ritual").addEventListener("click", function() {
+    alert("Game Time!");
+  });
